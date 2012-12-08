8aa6f5250d9c7143b84d8a3b1dd9046e25792d76
Shashank Teotia <steotia@thoughtworks.com>
2012-12-08 13:42:16 +0530

    sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GossipTrack::Application.config.secret_token = '1d15228c073eaee023b33bb7158d98f4debc08857cb03e66264c8afface14c9559cbb30fe94c66b3af256a98f3fc66cf6a57b5c47bc9c7950b319a00c5607a4c'
